ea3ec5fe2c3278b803b4165d005b449a37157f68
Your Name <you@example.com>
2023-02-13 17:43:21 +0100

    small change to vector

--- vector.h
+++ vector.h
@@ -6,7 +6,7 @@
 /*   By: lloison < lloison@student.42mulhouse.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:14:01 by lloison           #+#    #+#             */
-/*   Updated: 2023/02/13 13:17:18 by lloison          ###   ########.fr       */
+/*   Updated: 2023/02/13 17:42:54 by lloison          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ typedef struct s_pos
 
 typedef struct s_vector
 {
-	t_pos	start;
-	t_pos	end;
+	float	x;
+	float	y;
 	float	length;
 }	t_vector;
 
