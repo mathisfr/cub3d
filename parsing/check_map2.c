9158aaaaf8613b6b0e56869da91ea3d1a6d51890
Your Name <you@example.com>
2023-02-24 17:22:37 +0100

    Small fix on door parsing

--- parsing/check_map2.c
+++ parsing/check_map2.c
@@ -3,21 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   check_map2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matfranc <matfranc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lloison < lloison@student.42mulhouse.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 16:56:14 by lloison           #+#    #+#             */
-/*   Updated: 2023/02/23 18:44:13 by matfranc         ###   ########.fr       */
+/*   Updated: 2023/02/24 17:22:11 by lloison          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static t_bool	is_empty(char c)
+{
+	if (c == '0' || c == 'N' || c == 'E' || c == 'W' || c == 'S')
+		return (TRUE);
+	return (FALSE);
+}
+
 static t_bool	is_door_valid(t_map *map, int x, int y)
 {
 	if (map->map_arr[y - 1][x] == '1' && map->map_arr[y + 1][x] == '1'
-		&& map->map_arr[y][x - 1] == '0' && map->map_arr[y][x + 1] == '0')
+		&& is_empty(map->map_arr[y][x - 1])
+		&& is_empty(map->map_arr[y][x + 1]))
 		return (TRUE);
-	if (map->map_arr[y - 1][x] == '0' && map->map_arr[y + 1][x] == '0'
+	if (is_empty(map->map_arr[y - 1][x]) && is_empty(map->map_arr[y + 1][x])
 		&& map->map_arr[y][x - 1] == '1' && map->map_arr[y][x + 1] == '1')
 		return (TRUE);
 	return (FALSE);
